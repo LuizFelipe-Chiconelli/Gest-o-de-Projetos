bad997446f688b1d0229f91d14d7a0cb2d0c6c24
Luiz Felipe <lf5040870@gmail.com>
2025-06-15 18:04:19 -0300

    Update database
    Atualizando banco com melhoras

--- banco_gestaoprojetos.sql
+++ banco_gestaoprojetos.sql
@@ -61,8 +61,9 @@ CREATE TABLE `entrega` (
   `arquivo` varchar(120) DEFAULT NULL,
   `status` varchar(20) DEFAULT NULL,
   PRIMARY KEY (`id`),
-  KEY `fk_ent_proj` (`projeto_id`)
-) ENGINE=MyISAM AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  KEY `fk_entrega_projeto` (`projeto_id`),
+  CONSTRAINT `fk_entrega_projeto` FOREIGN KEY (`projeto_id`) REFERENCES `projeto` (`id`) ON DELETE SET NULL
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -71,7 +72,7 @@ CREATE TABLE `entrega` (
 
 LOCK TABLES `entrega` WRITE;
 /*!40000 ALTER TABLE `entrega` DISABLE KEYS */;
-INSERT INTO `entrega` VALUES (3,123132,'SAAFASD','2025-06-21','thumb-1920-1341150.png','Entregue');
+INSERT INTO `entrega` VALUES (3,2,'SAAFASD','2025-06-21','thumb-1920-1341150.png','Entregue');
 /*!40000 ALTER TABLE `entrega` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -84,7 +85,7 @@ DROP TABLE IF EXISTS `professor`;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `professor` (
   `id` int NOT NULL AUTO_INCREMENT,
-  `nome` varchar(60) DEFAULT NULL,
+  `nome` varchar(60) NOT NULL,
   `especialidade` varchar(50) NOT NULL,
   `email` varchar(150) NOT NULL,
   `area` varchar(50) DEFAULT NULL,
@@ -149,8 +150,10 @@ CREATE TABLE `projeto_aluno` (
   `aluno_id` int NOT NULL,
   `funcao` varchar(30) DEFAULT NULL,
   PRIMARY KEY (`projeto_id`,`aluno_id`),
-  KEY `fk_pa_aluno` (`aluno_id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  KEY `fk_projeto_aluno_aluno` (`aluno_id`),
+  CONSTRAINT `fk_projeto_aluno_aluno` FOREIGN KEY (`aluno_id`) REFERENCES `aluno` (`id`) ON DELETE CASCADE,
+  CONSTRAINT `fk_projeto_aluno_projeto` FOREIGN KEY (`projeto_id`) REFERENCES `projeto` (`id`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -159,7 +162,7 @@ CREATE TABLE `projeto_aluno` (
 
 LOCK TABLES `projeto_aluno` WRITE;
 /*!40000 ALTER TABLE `projeto_aluno` DISABLE KEYS */;
-INSERT INTO `projeto_aluno` VALUES (2,5,NULL),(2,4,NULL);
+INSERT INTO `projeto_aluno` VALUES (2,4,NULL),(2,5,NULL);
 /*!40000 ALTER TABLE `projeto_aluno` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -183,7 +186,7 @@ CREATE TABLE `reuniao` (
   PRIMARY KEY (`id`),
   KEY `fk_reuniao_projeto` (`projeto_id`),
   CONSTRAINT `fk_reuniao_projeto` FOREIGN KEY (`projeto_id`) REFERENCES `projeto` (`id`) ON DELETE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -204,14 +207,14 @@ DROP TABLE IF EXISTS `usuario`;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `usuario` (
   `id` int NOT NULL AUTO_INCREMENT,
-  `nome` varchar(60) DEFAULT NULL,
-  `email` varchar(150) DEFAULT NULL,
-  `senha` varchar(255) DEFAULT NULL,
+  `nome` varchar(60) NOT NULL,
+  `email` varchar(150) NOT NULL,
+  `senha` varchar(255) NOT NULL,
   `nivel` tinyint DEFAULT NULL,
   `statusRegistro` tinyint DEFAULT '1',
   PRIMARY KEY (`id`),
   UNIQUE KEY `email` (`email`)
-) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -223,10 +226,6 @@ LOCK TABLES `usuario` WRITE;
 INSERT INTO `usuario` VALUES (2,'admin','admin@gmail.com','$2y$12$juViAaPVr.asmio32DI/zeb4S.8b1oVjXP/RbZYblBYuviEJhj4ny',1,1);
 /*!40000 ALTER TABLE `usuario` ENABLE KEYS */;
 UNLOCK TABLES;
-
---
--- Dumping routines for database 'gestao_projetos'
---
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -237,4 +236,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2025-06-15 17:52:32
+-- Dump completed on 2025-06-15 18:03:29
